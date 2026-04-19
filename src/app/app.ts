98045d6e963733ca486608348dfbbf64f0fc792a
Gabriel <gabrielsgonc.dev@gmail.com>
2026-04-19 11:50:20 -0300

    feat: add main 'hero' content'

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./components/header/header";
+import { Main } from "./components/main/main";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Main],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
