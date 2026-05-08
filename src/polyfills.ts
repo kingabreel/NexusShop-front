4a96a89d288970e37392794739c35e4c1458971c
Gabriel <gabrielsgonc.dev@gmail.com>
2026-05-07 22:18:49 -0300

    config: add polyfills config

---
+++ src/polyfills.ts
@@ -0,0 +1,5 @@
+// Source - https://stackoverflow.com/a/51380307
+// Posted by Chellappan வ, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-05-06, License - CC BY-SA 4.0
+
+(window as any).global = window
