1e02a140d799304129f20014350d43840543cf10
Gabriel <gabrielsgonc.dev@gmail.com>
2026-05-29 00:39:55 -0300

    feat: search function in header + cleanup function + filtering by name

--- src/app/pages/search/search-page.ts
+++ src/app/pages/search/search-page.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { Product } from '../../shared/interface/product';
 import { NavFilter } from "../../components/nav-filter/nav-filter";
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ProductCard } from "../../components/cards/product-card/product-card";
+import { ProductService } from '../../shared/service/product-service';
+import { EventService } from '../../shared/service/search-event-service';
 
 @Component({
   selector: 'app-search-page',
@@ -18,6 +20,25 @@ export class SearchPage {
 
   currentPage = 1;
   pageSize = 8;
+
+  constructor(
+    private productService: ProductService,
+    private cdr: ChangeDetectorRef,
+    private eventService: EventService) { }
+
+  ngOnInit() {
+    this.eventService.search$.subscribe((query) => {
+      this.fetchProducts(query);
+    });
+
+    const searchQuery = localStorage.getItem('searchQuery');
+    if (searchQuery) {
+      this.fetchProducts(searchQuery);
+      localStorage.removeItem('searchQuery');
+    } else {
+      this.fetchProducts('');
+    }
+  }
 
   get totalPages(): number {
     return Math.ceil(this.products.length / this.pageSize);
@@ -42,4 +63,10 @@ export class SearchPage {
     }
   }
 
+  fetchProducts(query: string): void {
+    this.productService.getAll({ name: query }).subscribe((res) => {
+      this.products = res.data.content;
+      this.cdr.detectChanges();
+    });
+  }
 }
